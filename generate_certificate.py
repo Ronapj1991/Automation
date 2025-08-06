75298e1db43a67acf58bdd15e4ebcf407753239e
Ron Javellana <Ronapj1991@gmail.com>
2025-08-06 15:22:22 -0400

    refactored icon replacement

--- generate_certificate.py
+++ generate_certificate.py
@@ -15,6 +15,14 @@ def date_replace(current_date, new_date):
     page.draw_rect(current_date_rect, color=(1,1,1), fill=(1,1,1), width=1)
     page.insert_textbox(current_date_rect + (-20, -20, 20, 20), new_date, fontsize=17, fontname='heit', color=(0,0,0), align=1)
 
+def replace_icon(replacement):
+    try:
+        icon = page.get_image_bbox(images[0])
+        page.draw_rect(icon, color=(1,1,1), fill=(1,1,1), width=1)
+        page.insert_image(icon, filename=replacement)
+    except IndexError:
+        print("No image was found")
+
 try:
     cert = fitz.open(Path(r"C:\Users\RonJavellana\Downloads\PDF_Files\certificate.pdf"))
 except FileNotFoundError:
@@ -26,14 +34,7 @@ name_replace("Anita Bath", "John Doe")
 date_replace("12 May, 2025", "15 Feb, 2025")
 
 images = page.get_images(full=True)
-
-try:
-    icon = page.get_image_bbox(images[0])
-    page.draw_rect(icon, color=(1,1,1), fill=(1,1,1), width=1)
-    page.insert_image(icon, filename=Path(r"C:\Users\RonJavellana\Downloads\test.png"))
-except IndexError:
-    print("No image was found")
-
+replace_icon(Path(r"C:\Users\RonJavellana\Downloads\test.png"))
 
 cert.save(Path(r"C:\Users\RonJavellana\Downloads\PDF_Files\GeneratedCerts\new.pdf"))
 print("Training Certificate(s) generated")
