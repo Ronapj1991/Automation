a18aa5b0e5e44b431c8427b1d8fba07efca41644
Ron Javellana <Ronapj1991@gmail.com>
2025-08-06 13:51:22 -0400

    new folder and attachment types added

--- sort_files.py
+++ sort_files.py
@@ -5,7 +5,7 @@ import shutil
 
 folder = Path.home() / "Downloads"
 # Create subfolders in the Downloads directory to organize files
-subfolders = ["CSV_Files", "PDF_Files", "Video_Files", "Installers", "ZIP_archives"]
+subfolders = ["CSV_Files", "PDF_Files", "Video_Files", "Installers", "ZIP_archives", "Other"]
 
 for subfolder in subfolders:
     try:
@@ -18,13 +18,14 @@ for filename in os.listdir(folder):
     full_path = folder / filename
     if filename.endswith(".csv"):
         shutil.move(full_path, folder / "CSV_Files" / filename)
-    if filename.endswith(".pdf"):
+    elif filename.endswith(".pdf"):
         shutil.move(full_path, folder / "PDF_Files" / filename)
-    if filename.endswith(".mp4") or filename.endswith(".mov") or filename.endswith(".webm"):
+    elif filename.endswith(".mp4") or filename.endswith(".mov") or filename.endswith(".webm"):
        shutil.move(full_path, folder / "Video_files" / filename)
-    if filename.endswith(".exe") or filename.endswith(".msi"):
+    elif filename.endswith(".exe") or filename.endswith(".msi"):
         shutil.move(full_path, folder / "Installers" / filename)
-    if filename.endswith(".zip") or filename.endswith(".rar"):
+    elif filename.endswith(".zip") or filename.endswith(".rar"):
         shutil.move(full_path, folder / "ZIP_archives" / filename)
-
+    elif filename.endswith(".eml") or filename.endswith("docx") or filename.endswith("xml") or filename.endswith("dotx"):
+        shutil.move(full_path, folder / "Other" / filename)
 print("Files sorted successfully.")
